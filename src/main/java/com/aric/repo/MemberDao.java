41f886b092e06367ad3d1a5fe6b2b22d81b13ff7
dursunkoc <dursunKoc@gmail.com>
2013-01-16 22:53:29 +0200

    Initial commit

---
+++ src/main/java/com/aric/repo/MemberDao.java
@@ -0,0 +1,16 @@
+package com.aric.repo;
+
+import java.util.List;
+
+import com.aric.domain.Member;
+
+public interface MemberDao
+{
+    public Member findById(Long id);
+
+    public Member findByEmail(String email);
+
+    public List<Member> findAllOrderedByName();
+
+    public void register(Member member);
+}
